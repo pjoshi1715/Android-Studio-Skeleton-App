1f3bd55cf9de8e8be3774cd1a9e69b34b6b1bb8e
Elau2 <ethanlau03.2@gmail.com>
2019-10-11 21:09:47 -0700

    backup for Arav's screen

--- app/src/main/java/CSA/myapplication/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/CSA/myapplication/ui/notifications/NotificationsFragment.java
@@ -1,15 +1,20 @@
 package CSA.myapplication.ui.notifications;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
+import java.util.concurrent.TimeUnit;
 
 import CSA.myapplication.R;
 
@@ -28,6 +33,19 @@ public class NotificationsFragment extends Fragment {
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
+        });
+
+        final EditText text_compliment = root.findViewById(R.id.text_compliment);
+        final TextView textView2 = root.findViewById(R.id.textView2);
+        text_compliment.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+                textView2.setVisibility(View.VISIBLE);
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
         });
         return root;
     }
